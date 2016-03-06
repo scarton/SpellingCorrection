9a6a4ad8f540b69ec49c48d12b1f7561dac83499
Steve Carton <steve@chesbay.net>
2016-03-06 10:16:20 -0500

    initial git commit

---
+++ src/test/java/com/aitheras/spelling/SpellingTest.java
@@ -0,0 +1,28 @@
+package com.aitheras.spelling;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.common.base.Joiner;
+
+/**
+ * Unit test for spelling .
+ */
+public class SpellingTest {
+	private static Spelling speller;
+	@BeforeClass
+	public static void initSpeller() throws IOException {
+		speller = new Spelling();
+		speller.loadModels();
+	}
+	@Test
+	public void testSpell() {
+		String words = "teh quic borwn fxo jumpde voer the lazyz dogg";
+		String cwords = Joiner.on(" ").join(speller.correct(words.split(" ")));
+		assertTrue(cwords.equals("the quick brown fox jumper over the lazy dog"));
+	}
+}
